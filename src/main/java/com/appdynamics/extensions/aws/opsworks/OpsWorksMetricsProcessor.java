8e8c02c0df63a1013d4d97b8adf4f544ac9e2079
akshayAppd <akshay.srivastava@appdynamics.com>
2020-05-15 18:27:04 +0530

    Upgrade to aws commons 2.2.3.1

--- src/main/java/com/appdynamics/extensions/aws/opsworks/OpsWorksMetricsProcessor.java
+++ src/main/java/com/appdynamics/extensions/aws/opsworks/OpsWorksMetricsProcessor.java
@@ -8,19 +8,23 @@
 
 package com.appdynamics.extensions.aws.opsworks;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Pattern;
-
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
-import com.amazonaws.services.cloudwatch.model.Metric;
-import com.appdynamics.extensions.aws.config.MetricType;
+import com.amazonaws.services.cloudwatch.model.DimensionFilter;
+import com.appdynamics.extensions.aws.config.Dimension;
+import com.appdynamics.extensions.aws.config.IncludeMetric;
+import com.appdynamics.extensions.aws.dto.AWSMetric;
 import com.appdynamics.extensions.aws.metric.NamespaceMetricStatistics;
 import com.appdynamics.extensions.aws.metric.StatisticType;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessorHelper;
+import com.appdynamics.extensions.aws.predicate.MultiDimensionPredicate;
+import com.appdynamics.extensions.metrics.Metric;
+import com.google.common.collect.Lists;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @author Florencio Sarmiento
@@ -29,41 +33,54 @@ import com.appdynamics.extensions.aws.metric.processors.MetricsProcessorHelper;
 public class OpsWorksMetricsProcessor implements MetricsProcessor {
 	
 	private static final String NAMESPACE = "AWS/OpsWorks";
-	
-	private static final String[] DIMENSIONS = {"StackId", "LayerId", "InstanceId"};
-	
-	private List<MetricType> metricTypes;
-	
-	private Pattern excludeMetricsPattern;
-	
-	public OpsWorksMetricsProcessor(List<MetricType> metricTypes,
-			Set<String> excludeMetrics) {
-		this.metricTypes = metricTypes;
-		this.excludeMetricsPattern = MetricsProcessorHelper.createPattern(excludeMetrics);
+
+	private List<IncludeMetric> includeMetrics;
+
+	private List<Dimension> dimensions;
+
+	public OpsWorksMetricsProcessor(List<IncludeMetric> includeMetrics, List<Dimension> dimensions) {
+		this.includeMetrics = includeMetrics;
+		this.dimensions = dimensions;
 	}
 
-	public List<Metric> getMetrics(AmazonCloudWatch awsCloudWatch, String accountName) {
-		return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch, 
-				NAMESPACE, 
-				excludeMetricsPattern);
+	public List<AWSMetric> getMetrics(AmazonCloudWatch awsCloudWatch, String accountName, LongAdder awsRequestsCounter) {
+		List<DimensionFilter> dimensionFilters = getDimensionFilters();
+
+		MultiDimensionPredicate predicate = new MultiDimensionPredicate(dimensions);
+
+		return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch,awsRequestsCounter,
+				NAMESPACE,
+				includeMetrics,
+				dimensionFilters,
+				predicate);
 	}
 	
-	public StatisticType getStatisticType(Metric metric) {
-		return MetricsProcessorHelper.getStatisticType(metric, metricTypes);
+	public StatisticType getStatisticType(AWSMetric metric) {
+		return MetricsProcessorHelper.getStatisticType(metric.getIncludeMetric(), includeMetrics);
 	}
-	
-	public Map<String, Double> createMetricStatsMapForUpload(NamespaceMetricStatistics namespaceMetricStats) {
+
+	public List<Metric> createMetricStatsMapForUpload(NamespaceMetricStatistics namespaceMetricStats) {
 		Map<String, String> dimensionToMetricPathNameDictionary = new HashMap<String, String>();
-		dimensionToMetricPathNameDictionary.put(DIMENSIONS[0], "Stack");
-		dimensionToMetricPathNameDictionary.put(DIMENSIONS[1], "Layer");
-		dimensionToMetricPathNameDictionary.put(DIMENSIONS[2], "Instance");
-		
-		return MetricsProcessorHelper.createMetricStatsMapForUpload(namespaceMetricStats, 
+		for (Dimension dimension : dimensions) {
+			dimensionToMetricPathNameDictionary.put(dimension.getName(), dimension.getDisplayName());
+		}
+		return MetricsProcessorHelper.createMetricStatsMapForUpload(namespaceMetricStats,
 				dimensionToMetricPathNameDictionary, false);
 	}
-	
+
 	public String getNamespace() {
 		return NAMESPACE;
+	}
+
+	private List<DimensionFilter> getDimensionFilters() {
+
+		List<DimensionFilter> dimensionFilters = Lists.newArrayList();
+		for (Dimension dimension : dimensions) {
+			DimensionFilter opsDimensionFilter = new DimensionFilter();
+			opsDimensionFilter.withName(dimension.getName());
+			dimensionFilters.add(opsDimensionFilter);
+		}
+		return dimensionFilters;
 	}
 
 }
--- src/main/java/com/appdynamics/extensions/aws/opsworks/OpsWorksMonitor.java
+++ src/main/java/com/appdynamics/extensions/aws/opsworks/OpsWorksMonitor.java
@@ -8,15 +8,18 @@
 
 package com.appdynamics.extensions.aws.opsworks;
 
-import static com.appdynamics.extensions.aws.Constants.METRIC_PATH_SEPARATOR;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
-
 import com.appdynamics.extensions.aws.SingleNamespaceCloudwatchMonitor;
 import com.appdynamics.extensions.aws.collectors.NamespaceMetricStatisticsCollector;
 import com.appdynamics.extensions.aws.config.Configuration;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
+import com.appdynamics.extensions.logging.ExtensionsLoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.appdynamics.extensions.aws.Constants.METRIC_PATH_SEPARATOR;
 
 /**
  * @author Florencio Sarmiento
@@ -24,10 +27,12 @@ import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
  */
 public class OpsWorksMonitor extends SingleNamespaceCloudwatchMonitor<Configuration> {
 	
-	private static final Logger LOGGER = Logger.getLogger("com.singularity.extensions.aws.OpsWorksMonitor");
+	private static final Logger LOGGER = ExtensionsLoggerFactory.getLogger(OpsWorksMonitor.class);
 
 	private static final String DEFAULT_METRIC_PREFIX = String.format("%s%s%s%s", 
 			"Custom Metrics", METRIC_PATH_SEPARATOR, "Amazon OpsWorks", METRIC_PATH_SEPARATOR);
+
+	private static final String MONITOR_NAME = "AWSOpsWorksMonitor";
 	
 	public OpsWorksMonitor() {
 		super(Configuration.class);
@@ -44,8 +49,9 @@ public class OpsWorksMonitor extends SingleNamespaceCloudwatchMonitor<Configurat
 				.Builder(config.getAccounts(),
 						config.getConcurrencyConfig(), 
 						config.getMetricsConfig(),
-						metricsProcessor)
-				.withCredentialsEncryptionConfig(config.getCredentialsDecryptionConfig())
+						metricsProcessor,
+				config.getMetricPrefix())
+				.withCredentialsDecryptionConfig(config.getCredentialsDecryptionConfig())
 				.withProxyConfig(config.getProxyConfig())
 				.build();
 	}
@@ -54,17 +60,26 @@ public class OpsWorksMonitor extends SingleNamespaceCloudwatchMonitor<Configurat
 	protected Logger getLogger() {
 		return LOGGER;
 	}
-	
+
 	@Override
-	protected String getMetricPrefix(Configuration config) {
-		return StringUtils.isNotBlank(config.getMetricPrefix()) ? 
-				config.getMetricPrefix() : DEFAULT_METRIC_PREFIX;
+	protected String getDefaultMetricPrefix() {
+		return  DEFAULT_METRIC_PREFIX;
+	}
+
+	@Override
+	public String getMonitorName() {
+		return MONITOR_NAME;
+	}
+
+	@Override
+	protected List<Map<String, ?>> getServers() {
+		return new ArrayList<Map<String, ?>>();
 	}
 
 	private MetricsProcessor createMetricsProcessor(Configuration config) {
 		return new OpsWorksMetricsProcessor(
-				config.getMetricsConfig().getMetricTypes(), 
-				config.getMetricsConfig().getExcludeMetrics());
+				config.getMetricsConfig().getIncludeMetrics(),
+				config.getDimensions());
 	}
 
 }
